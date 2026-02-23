993e90bb2b9564bf34afe43027f6ef3c08376ab4
littleStinker <72678136+LittleStinkerGuy@users.noreply.github.com>
2026-02-23 15:51:57 -0800

    refactor LoggedTalonFX to enhance telemetry logging and remove SmartDashboard dependencies

--- src/main/java/frc/robot/util/LoggedTalonFX.java
+++ src/main/java/frc/robot/util/LoggedTalonFX.java
@@ -6,12 +6,11 @@ import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.StatusCode;
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.ControlModeValue;
 import com.ctre.phoenix6.signals.ForwardLimitValue;
 import com.ctre.phoenix6.signals.ReverseLimitValue;
 
-import edu.wpi.first.util.datalog.BooleanLogEntry;
 import edu.wpi.first.util.datalog.DataLog;
-import edu.wpi.first.util.datalog.DoubleLogEntry;
 import edu.wpi.first.units.CurrentUnit;
 import edu.wpi.first.units.TorqueUnit;
 import edu.wpi.first.units.Units;
@@ -24,7 +23,7 @@ import edu.wpi.first.units.measure.Temperature;
 import edu.wpi.first.units.measure.Torque;
 import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import org.littletonrobotics.junction.Logger;
 
 public class LoggedTalonFX extends TalonFX {
     enum TelemetryLevel {
@@ -41,13 +40,12 @@ public class LoggedTalonFX extends TalonFX {
     private static final TelemetryLevel DEFAULT_TELEMETRY_LEVEL = TelemetryLevel.BASIC;
     private static final double DEFAULT_UPDATE_HZ = 10.0;
 
-    private final String dashboardKey; 
     private final String logPrefix;
 
     public TelemetryLevel dashboardTelemetryLevel = DEFAULT_TELEMETRY_LEVEL;
     public TelemetryLevel logTelemetryLevel = DEFAULT_TELEMETRY_LEVEL;
 
-    public double updatesPerSec = 1.0 / DEFAULT_UPDATE_HZ;
+    public double updatesPerSec = DEFAULT_UPDATE_HZ;
     private double lastUpdateTime = 0;
 
     // ===== Cached signals (refresh once, then read many) =====
@@ -57,6 +55,7 @@ public class LoggedTalonFX extends TalonFX {
 
     private final StatusSignal<Double> dutyCycle = getDutyCycle();
     private final StatusSignal<Voltage> supplyVoltage = getSupplyVoltage();
+    private final StatusSignal<Voltage> appliedVoltage = getMotorVoltage();
     private final StatusSignal<Current> supplyCurrent = getSupplyCurrent();
     private final StatusSignal<Current> statorCurrent = getStatorCurrent();
 
@@ -67,6 +66,8 @@ public class LoggedTalonFX extends TalonFX {
 
     private final StatusSignal<Double> closedLoopReference = getClosedLoopReference();
     private final StatusSignal<Double> closedLoopError = getClosedLoopError();
+
+    private final StatusSignal<ControlModeValue> controlMode = getControlMode();
 
     private final StatusSignal<ForwardLimitValue> forwardLimit = getForwardLimit();
     private final StatusSignal<ReverseLimitValue> reverseLimit = getReverseLimit();
@@ -82,68 +83,7 @@ public class LoggedTalonFX extends TalonFX {
 
     public LoggedTalonFX(int deviceId, String canBus, String dashboardKey) {
         super(deviceId, canBus);
-        this.logPrefix = "/TalonFX/" + dashboardKey.replace("/", "_").replace(" ", "_");
-        this.dashboardKey = dashboardKey;
-    }
-
-    public void setupDashboard() {
-        SmartDashboard.putNumber(dashboardKey + "/Position", 0.0);
-        SmartDashboard.putNumber(dashboardKey + "/Velocity", 0.0);
-        SmartDashboard.putNumber(dashboardKey + "/Acceleration", 0.0);
-
-        SmartDashboard.putNumber(dashboardKey + "/DutyCycle", 0.0);
-        SmartDashboard.putNumber(dashboardKey + "/SupplyVoltage", 0.0);
-        SmartDashboard.putNumber(dashboardKey + "/SupplyCurrent", 0.0);
-        SmartDashboard.putNumber(dashboardKey + "/StatorCurrent", 0.0);
-
-        SmartDashboard.putNumber(dashboardKey + "/TempC", 0.0);
-
-        SmartDashboard.putNumber(dashboardKey + "/TorqueCurrent", 0.0);
-        SmartDashboard.putNumber(dashboardKey + "/TorqueNm", 0.0);
-
-        SmartDashboard.putNumber(dashboardKey + "/ClosedLoopRef", 0.0);
-        SmartDashboard.putNumber(dashboardKey + "/ClosedLoopError", 0.0);
-
-        SmartDashboard.putBoolean(dashboardKey + "/ForwardLimit", false);
-        SmartDashboard.putBoolean(dashboardKey + "/ReverseLimit", false);
-
-        SmartDashboard.putBoolean(dashboardKey + "/Fault/Brownout", false);
-        SmartDashboard.putBoolean(dashboardKey + "/Fault/Hardware", false);
-        SmartDashboard.putBoolean(dashboardKey + "/Fault/BootDuringEnable", false);
-    }
-
-    /** Call once in robotInit after DataLogManager.start(). */
-    public void setupLog(DataLog log) {
-        if (log == null) {
-            logReady = false;
-            return;
-        }
-
-        posLog = new DoubleLogEntry(log, logPrefix + "/Position");
-        velLog = new DoubleLogEntry(log, logPrefix + "/Velocity");
-        accelLog = new DoubleLogEntry(log, logPrefix + "/Acceleration");
-
-        dutyLog = new DoubleLogEntry(log, logPrefix + "/DutyCycle");
-        voltsLog = new DoubleLogEntry(log, logPrefix + "/SupplyVoltage");
-        supCurLog = new DoubleLogEntry(log, logPrefix + "/SupplyCurrent");
-        statCurLog = new DoubleLogEntry(log, logPrefix + "/StatorCurrent");
-
-        tempLog = new DoubleLogEntry(log, logPrefix + "/TempC");
-
-        tqCurLog = new DoubleLogEntry(log, logPrefix + "/TorqueCurrent");
-        tqNmLog = new DoubleLogEntry(log, logPrefix + "/TorqueNm");
-
-        clRefLog = new DoubleLogEntry(log, logPrefix + "/ClosedLoopRef");
-        clErrLog = new DoubleLogEntry(log, logPrefix + "/ClosedLoopError");
-
-        fwdLimitLog = new BooleanLogEntry(log, logPrefix + "/ForwardLimit");
-        revLimitLog = new BooleanLogEntry(log, logPrefix + "/ReverseLimit");
-
-        brownoutLog = new BooleanLogEntry(log, logPrefix + "/Fault/Brownout");
-        hardwareLog = new BooleanLogEntry(log, logPrefix + "/Fault/Hardware");
-        bootEnableLog = new BooleanLogEntry(log, logPrefix + "/Fault/BootDuringEnable");
-
-        logReady = true;
+        this.logPrefix = "TalonFX/" + dashboardKey.replace("/", "_").replace(" ", "_");
     }
 
     /**
@@ -159,6 +99,7 @@ public class LoggedTalonFX extends TalonFX {
                 acceleration,
                 dutyCycle,
                 supplyVoltage,
+                appliedVoltage,
                 supplyCurrent,
                 statorCurrent,
                 deviceTemp,
@@ -166,6 +107,7 @@ public class LoggedTalonFX extends TalonFX {
                 motorKt,
                 closedLoopReference,
                 closedLoopError,
+                controlMode,
                 forwardLimit,
                 reverseLimit,
                 faultBridgeBrownout,
@@ -174,116 +116,54 @@ public class LoggedTalonFX extends TalonFX {
     }
 
     /**
-     * Publish the latest signal values to SmartDashboard (rate limited by
-     * configuration).
+     * Publish the latest signal values to AdvantageKit (rate limited by
+     * configuration). These values can be sent to network tables and WPILOG
+     * simultaneously by configuring data receivers in Robot.
      */
     public void updateDashboard() {
-        if (!shouldRunDashboardUpdate()) {
-            return;
-        }
         refreshSignals();
 
-        if (shouldIncludeOnDashboard(TelemetryLevel.BASIC)) {
-            SmartDashboard.putNumber(dashboardKey + "/Position", position.getValue().in(Units.Radians));
-            SmartDashboard.putNumber(dashboardKey + "/Velocity", velocity.getValue().in(Units.RadiansPerSecond));
-            SmartDashboard.putNumber(dashboardKey + "/DutyCycle", dutyCycle.getValue());
+        TelemetryLevel telemetryLevel = getEffectiveTelemetryLevel();
+        if (telemetryLevel.includes(TelemetryLevel.BASIC)) {
+            Logger.recordOutput(logPrefix + "/Position", position.getValue().in(Units.Radians));
+            Logger.recordOutput(logPrefix + "/Velocity", velocity.getValue().in(Units.RadiansPerSecond));
+            Logger.recordOutput(logPrefix + "/DutyCycle", dutyCycle.getValue());
+            Logger.recordOutput(logPrefix + "/AppliedVoltage", appliedVoltage.getValue().in(Units.Volts));
+            Logger.recordOutput(logPrefix + "/ControlMode", (double) controlMode.getValue().ordinal());
 
-            SmartDashboard.putBoolean(dashboardKey + "/ForwardLimit", isForwardLimitClosed());
-            SmartDashboard.putBoolean(dashboardKey + "/ReverseLimit", isReverseLimitClosed());
+            Logger.recordOutput(logPrefix + "/ForwardLimit", isForwardLimitClosed());
+            Logger.recordOutput(logPrefix + "/ReverseLimit", isReverseLimitClosed());
 
-            SmartDashboard.putBoolean(dashboardKey + "/Fault/Brownout", faultBridgeBrownout.getValue());
-            SmartDashboard.putBoolean(dashboardKey + "/Fault/Hardware", faultHardware.getValue());
-            SmartDashboard.putBoolean(dashboardKey + "/Fault/BootDuringEnable", faultBootDuringEnable.getValue());
+            Logger.recordOutput(logPrefix + "/Fault/Brownout", faultBridgeBrownout.getValue());
+            Logger.recordOutput(logPrefix + "/Fault/Hardware", faultHardware.getValue());
+            Logger.recordOutput(logPrefix + "/Fault/BootDuringEnable", faultBootDuringEnable.getValue());
         }
 
-        if (shouldIncludeOnDashboard(TelemetryLevel.STANDARD)) {
-            SmartDashboard.putNumber(
-                    dashboardKey + "/Acceleration", acceleration.getValue().in(Units.RadiansPerSecondPerSecond));
-            SmartDashboard.putNumber(dashboardKey + "/SupplyVoltage", supplyVoltage.getValue().in(Units.Volts));
-            SmartDashboard.putNumber(dashboardKey + "/SupplyCurrent", supplyCurrent.getValue().in(Units.Amps));
+        if (telemetryLevel.includes(TelemetryLevel.STANDARD)) {
+            Logger.recordOutput(
+                    logPrefix + "/Acceleration", acceleration.getValue().in(Units.RadiansPerSecondPerSecond));
+            Logger.recordOutput(logPrefix + "/SupplyVoltage", supplyVoltage.getValue().in(Units.Volts));
+            Logger.recordOutput(logPrefix + "/SupplyCurrent", supplyCurrent.getValue().in(Units.Amps));
         }
 
-        if (shouldIncludeOnDashboard(TelemetryLevel.DETAILED)) {
-            SmartDashboard.putNumber(dashboardKey + "/StatorCurrent", statorCurrent.getValue().in(Units.Amps));
-            SmartDashboard.putNumber(dashboardKey + "/TempC", deviceTemp.getValue().in(Units.Celsius));
+        if (telemetryLevel.includes(TelemetryLevel.DETAILED)) {
+            Logger.recordOutput(logPrefix + "/StatorCurrent", statorCurrent.getValue().in(Units.Amps));
+            Logger.recordOutput(logPrefix + "/TempC", deviceTemp.getValue().in(Units.Celsius));
         }
 
-        if (shouldIncludeOnDashboard(TelemetryLevel.FULL)) {
-            SmartDashboard.putNumber(dashboardKey + "/TorqueCurrent", torqueCurrent.getValue().in(Units.Amps));
-            SmartDashboard.putNumber(dashboardKey + "/TorqueNm", getTorque().in(NewtonMeters));
-
-            SmartDashboard.putNumber(dashboardKey + "/ClosedLoopRef", closedLoopReference.getValue());
-            SmartDashboard.putNumber(dashboardKey + "/ClosedLoopError", closedLoopError.getValue());
-        }
-    }
-
-    /**
-     * Append the latest signal values to WPILib DataLog (rate limited by
-     * configuration).
-     */
-    public void updateLog() {
-        if (!logReady || !shouldRunLogUpdate()) {
-            return;
-        }
-
-        refreshSignals();
-        if (shouldIncludeInLog(TelemetryLevel.BASIC)) {
-            posLog.append(position.getValue().in(Units.Radians));
-            velLog.append(velocity.getValue().in(Units.RadiansPerSecond));
-            dutyLog.append(dutyCycle.getValue());
-
-            fwdLimitLog.append(isForwardLimitClosed());
-            revLimitLog.append(isReverseLimitClosed());
-
-            brownoutLog.append(faultBridgeBrownout.getValue());
-            hardwareLog.append(faultHardware.getValue());
-            bootEnableLog.append(faultBootDuringEnable.getValue());
-        }
-
-        if (shouldIncludeInLog(TelemetryLevel.STANDARD)) {
-            accelLog.append(acceleration.getValue().in(Units.RadiansPerSecondPerSecond));
-            voltsLog.append(supplyVoltage.getValue().in(Units.Volts));
-            supCurLog.append(supplyCurrent.getValue().in(Units.Amps));
-        }
-
-        if (shouldIncludeInLog(TelemetryLevel.DETAILED)) {
-            statCurLog.append(statorCurrent.getValue().in(Units.Amps));
-            tempLog.append(deviceTemp.getValue().in(Units.Celsius));
-        }
-
-        if (shouldIncludeInLog(TelemetryLevel.FULL)) {
-            tqCurLog.append(torqueCurrent.getValue().in(Units.Amps));
-            tqNmLog.append(getTorque().in(NewtonMeters));
-
-            clRefLog.append(closedLoopReference.getValue());
-            clErrLog.append(closedLoopError.getValue());
+        if (telemetryLevel.includes(TelemetryLevel.FULL)) {
+            Logger.recordOutput(logPrefix + "/TorqueCurrent", torqueCurrent.getValue().in(Units.Amps));
+            Logger.recordOutput(logPrefix + "/TorqueNm", getTorque().in(NewtonMeters));
+            Logger.recordOutput(logPrefix + "/MotorKtNmPerAmp", getMotorKtNmPerAmp());
+            Logger.recordOutput(logPrefix + "/ClosedLoopRef", closedLoopReference.getValue());
+            Logger.recordOutput(logPrefix + "/ClosedLoopError", closedLoopError.getValue());
         }
     }
 
-    private boolean shouldIncludeOnDashboard(TelemetryLevel required) {
-        return dashboardTelemetryLevel.includes(required);
-    }
-
-    private boolean shouldIncludeInLog(TelemetryLevel required) {
-        return logTelemetryLevel.includes(required);
-    }
-
-    private boolean shouldRunDashboardUpdate() {
-        double now = Timer.getFPGATimestamp();
-        if (now - lastDashboardUpdateTime < dashboardUpdatePerSec) {
-            return false;
-        }
-        lastDashboardUpdateTime = now;
-        return true;
-    }
-
-    private boolean shouldRunLogUpdate() {
-        double now = Timer.getFPGATimestamp();
-        if (now - lastLogUpdateTime < logUpdatePerSec) {
-            return false;
-        }
-        lastLogUpdateTime = now;
-        return true;
+    private TelemetryLevel getEffectiveTelemetryLevel() {
+        return dashboardTelemetryLevel.ordinal() >= logTelemetryLevel.ordinal()
+                ? dashboardTelemetryLevel
+                : logTelemetryLevel;
     }
 
     private boolean isForwardLimitClosed() {
@@ -298,5 +178,9 @@ public class LoggedTalonFX extends TalonFX {
         Per<TorqueUnit, CurrentUnit> kt = motorKt.getValue();
         Current tCurrent = torqueCurrent.getValue();
         return (Torque) kt.timesDivisor(tCurrent);
+    }
+
+    private double getMotorKtNmPerAmp() {
+        return motorKt.getValue().timesDivisor(Units.Amps.of(1.0)).in(NewtonMeters);
     }
 }
